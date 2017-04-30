09fe0d20efe04820791a3e8cc7cfb118c1e31e12
gforenze <idealum_05@hotmail.com>
2017-04-30 09:08:38 -0500

    Add files via upload

---
+++ SENSOR_DE_PULSO.ino
@@ -0,0 +1,29 @@
+int pulso=0;
+int pulmin;
+int x;
+
+void setup (){ 
+  Serial.begin(9600);
+  pinMode(A5,INPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+}
+
+void loop (){
+  
+  pulso= analogRead (A5);
+  
+  if (pulso>=660){
+    digitalWrite (11,HIGH);
+    delay  (30);
+    digitalWrite(11,LOW);
+  }  
+  if (pulso>=200){
+    digitalWrite (12,HIGH);
+    delay  (30);
+    digitalWrite(12,LOW); 
+
+ delay(50);
+ Serial.println(pulso);
+  }   }
+
---
+++ SENSOR_DE_TEMPERATURA.ino
@@ -0,0 +1,14 @@
+float tempC;
+int tempPin=0;
+void setup()
+{
+Serial.begin(9600);
+}
+void loop()
+{
+tempC=analogRead(tempPin);
+tempC=((5.0*tempC * 100.0)/1024.0); 
+Serial.print(tempC);
+Serial.print(" grados Celsius\n");
+delay(5000);
+}
